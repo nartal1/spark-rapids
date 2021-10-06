85a7176350eaff4cac57be80b9b0e5369265dbf4
Niranjan Artal <nartal@nvidia.com>
2021-10-06 16:54:03 -0700

    print uniq commits
    Signed-off-by: Niranjan Artal <nartal@nvidia.com>

--- scripts/audit-spark-3.3.sh
+++ scripts/audit-spark-3.3.sh
@@ -97,14 +97,14 @@ else
     ## Below steps filter commit header messages, sorts and saves only uniq commits that needs to be audited in commits.to.audit.3.3 file
     cat b3.2.log | awk '{$1 = "";print $0}' > b3.2.filter.log
     cat b3.3.log | awk '{$1 = "";print $0}' > b3.3.filter.log
-    echo "b3.2. filter.log is"
-    cat b3.2.filter.log
-    echo "b3.3.filter.log"
-    cat b3.3.filter.log
-    eval "cat b3.3.filter.log b3.2.filter.log | sort | uniq -c | sort  | awk '/^[[:space:]]*1/{$1 = "";print $0}' > uniqcommits.log"
-    eval "cat b3.2.filter.log | sort > b3.2.filter.sorted.log"
-    eval "cat b3.3.filter.log | sort > b3.3.filter.sorted.log"
-    eval "cat uniqcommits.log | sort > uniqcommits.sorted.log"
+    cat b3.3.filter.log b3.2.filter.log | sort | uniq -c | sort  | awk '/^[[:space:]]*1/{$1 = "";print $0}' > uniqcommits.log
+    cat b3.2.filter.log | sort > b3.2.filter.sorted.log
+    cat b3.3.filter.log | sort > b3.3.filter.sorted.log
+    cat uniqcommits.log | sort > uniqcommits.sorted.log
+    echo "uniqcommits is "
+    cat uniqcommits.log
+    echo "uniq sorted commits is"
+    cat uniqcommits.sorted.log
     eval "comm -12 b3.2.filter.sorted.log uniqcommits.sorted.log | wc -l"
     eval "comm -12 b3.3.filter.sorted.log uniqcommits.sorted.log > commits.to.audit.3.3"
     eval "sed -i 's/\[/\\[/g' commits.to.audit.3.3"
