4f97eeb2a80454d25e1a2cc86ebdfca3d4d631b6
Niranjan Artal <nartal@nvidia.com>
2024-04-02 14:44:18 -0700

    update commit

--- scripts/audit-spark.sh
+++ scripts/audit-spark.sh
@@ -33,7 +33,7 @@ lastcommit=""
 basebranch="master"
 tag="branch-3.2"
 commonancestor=""
-REF=${REF:-"main"}
+REF=${REF:-"test-audit-script"}
 REF=main
 while getopts v:b:t:c: flag
 do
@@ -53,7 +53,7 @@ git clone https://github.com/apache/spark.git $SPARK_TREE
 
 if [ -f "$lastcommit" ]; then
     cd ${SPARK_TREE}
-    latestcommit=`cat ${lastcommit}`
+    latestcommit="20b593811dc"
     git checkout $basebranch
     git log --oneline HEAD...$latestcommit -- sql/core/src/main sql/catalyst/src/main \
 	    core/src/main/scala/org/apache/spark/shuffle core/src/main/scala/org/apache/spark/storage | tee ${COMMIT_DIFF_LOG}
