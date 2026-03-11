e3517ea757bfc41848ccd2f0d14c7a0e313b6018
Niranjan Artal <nartal@nvidia.com>
2026-03-11 23:41:37 +0000

    Fix Db-17.3 integration tests for date_time_test.py
    Signed-off-by: Niranjan Artal <nartal@nvidia.com>

--- sql-plugin/src/main/scala/org/apache/spark/sql/rapids/GpuFileSourceScanExec.scala
+++ sql-plugin/src/main/scala/org/apache/spark/sql/rapids/GpuFileSourceScanExec.scala
@@ -654,13 +654,13 @@ case class GpuFileSourceScanExec(
 object GpuFileSourceScanExec {
   def tagSupport(meta: SparkPlanMeta[FileSourceScanExec]): Unit = {
     val cls = meta.wrapped.relation.fileFormat.getClass
-    if (cls == classOf[CSVFileFormat]) {
+    if (classOf[CSVFileFormat].isAssignableFrom(cls)) {
       GpuReadCSVFileFormat.tagSupport(meta)
     } else if (GpuOrcFileFormat.isSparkOrcFormat(cls)) {
       GpuReadOrcFileFormat.tagSupport(meta)
-    } else if (cls == classOf[ParquetFileFormat]) {
+    } else if (classOf[ParquetFileFormat].isAssignableFrom(cls)) {
       GpuReadParquetFileFormat.tagSupport(meta)
-    } else if (cls == classOf[JsonFileFormat]) {
+    } else if (classOf[JsonFileFormat].isAssignableFrom(cls)) {
       GpuReadJsonFileFormat.tagSupport(meta)
     } else if (ExternalSource.isSupportedFormat(cls)) {
       ExternalSource.tagSupportForGpuFileSourceScan(meta)
@@ -672,13 +672,13 @@ object GpuFileSourceScanExec {
   def convertFileFormat(relation: HadoopFsRelation): FileFormat = {
     val format = relation.fileFormat
     val cls = format.getClass
-    if (cls == classOf[CSVFileFormat]) {
+    if (classOf[CSVFileFormat].isAssignableFrom(cls)) {
       new GpuReadCSVFileFormat
     } else if (GpuOrcFileFormat.isSparkOrcFormat(cls)) {
       new GpuReadOrcFileFormat
-    } else if (cls == classOf[ParquetFileFormat]) {
+    } else if (classOf[ParquetFileFormat].isAssignableFrom(cls)) {
       new GpuReadParquetFileFormat
-    } else if (cls == classOf[JsonFileFormat]) {
+    } else if (classOf[JsonFileFormat].isAssignableFrom(cls)) {
       new GpuReadJsonFileFormat
     } else if (ExternalSource.isSupportedFormat(cls)) {
       ExternalSource.getReadFileFormat(relation)
