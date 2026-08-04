6265fd4e690aa75706560014631e48b0bc4e090c
Niranjan Artal <nartal@nvidia.com>
2026-08-04 22:31:05 +0000

    Support native Delta OPTIMIZE writes on GPU
    Signed-off-by: Niranjan Artal <nartal@nvidia.com>

--- integration_tests/src/main/python/delta_lake_write_test.py
+++ integration_tests/src/main/python/delta_lake_write_test.py
@@ -1805,7 +1805,7 @@ def test_delta_write_column_name_mapping(spark_tmp_path, mapping):
 
 # Hash aggregate can be used in a metadata query for compaction which completely falls back
 compaction_allow = "HashAggregateExec"
-if is_databricks_runtime():
+if is_databricks_runtime() and not (is_databricks143() or is_databricks173_or_later()):
     # compaction can fallback due to unsupported WriteIntoDeltaCommand
     # tracked by https://github.com/NVIDIA/spark-rapids/issues/11169
     compaction_allow += "," + delta_write_fallback_allow
